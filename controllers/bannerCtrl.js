4338cd2aee466a7137a6e481e8b8b33e6b6abb91
hasanzade7832 <hasanzade.hoseyn27@gmail.com>
2024-03-29 16:27:12 +0330

    work on banner

--- controllers/bannerCtrl.js
+++ controllers/bannerCtrl.js
@@ -1,40 +1,103 @@
-const Banners = require('../models/banner');
+const Banners = require("../models/banner");
+const { updateOne, deleteOne } = require("mongoose"); // اضافه کردن متدهای مورد نیاز از mongoose
 
-const getAllBanner = async (req , res) => {
-    try{
-        const AllBanners = await Banners.find();
-        res.status(200).json(AllBanners);
+const getAllBanner = async (req, res) => {
+  try {
+    let banners;
+    if (req.query.pn) {
+      const paginate = 2;
+      const pageNumber = req.query.pn;
+      banners = await Banners.find()
+        .sort({ _id: -1 })
+        .skip((pageNumber - 1) * paginate)
+        .limit(paginate);
+    } else {
+      banners = await Banners.find();
     }
-    catch (err) {
+    const allBannersNum = await Banners.countDocuments();
+    res.status(200).json({ banners, allBannersNum }); // ارسال لیست بنرها به همراه تعداد آن‌ها
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ msg: "error" });
+  }
+};
+
+const newBanner = async (req, res) => {
+  try {
+    const newBanner = new Banners({
+      image: req.body.image,
+      imageAlt: req.body.imageAlt,
+      situation: req.body.situation,
+      link: req.body.link,
+      date: new Date().toLocaleDateString("fa-IR", {
+        hour: "2-digit",
+        minute: "2-digit",
+      }),
+    });
+    newBanner
+      .save()
+      .then((d) => {
+        res.status(200).json({ msg: "بنر با موفقیت ذخیره شد" });
+      })
+      .catch((err) => {
         console.log(err);
-        res.status(400).json({msg:"error"})
-    }
-}
-module.exports.getAllBanner = getAllBanner
+        res.status(400).json({ msg: "هنگام ذخیره بنر مشکلی پیش آمد" });
+      });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ msg: "error" });
+  }
+};
 
+const updateBanner = async (req, res) => {
+  try {
+    await Banners.updateOne(
+      // استفاده از updateOne برای بروزرسانی بنر
+      { _id: req.body.goalId },
+      {
+        $set: {
+          image: req.body.image,
+          imageAlt: req.body.imageAlt,
+          situation: req.body.situation,
+          link: req.body.link,
+          date: new Date().toLocaleDateString("fa-IR", {
+            hour: "2-digit",
+            minute: "2-digit",
+          }),
+        },
+      }
+    );
+    res.status(200).json({ msg: "بنر با موفقیت بروز رسانی شد" });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ msg: "error" });
+  }
+};
 
-const newBanner = async (req , res) => {
-    try{
-        const newBanner= new Banners({
-            image:req.body.image,
-            imageAlt:req.body.imageAlt,
-            situation:req.body.situation,
-            link:req.body.link,
-            date:new Date().toLocaleDateString('fa-IR',{hour:"2-digit",minute:"2-digit"}),
-        });
-        newBanner.save()
-        .then(d=>{
-            res.status(200).json({msg:"بنر با موفقیت ذخیره شد"})
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(400).json({msg:"هنگام ذخیره بنر مشکلی پیش آمد"})
-        })
+const deleteBanner = async (req, res) => {
+  try {
+    await Banners.deleteOne({ _id: req.body.goalId }); // استفاده از deleteOne برای حذف بنر
+    res.status(200).json({ msg: "بنر با موفقیت حذف شد" });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ msg: "error" });
+  }
+};
 
-    }
-    catch (err) {
-        console.log(err);
-        res.status(400).json({msg:"error"})
-    }
-}
-module.exports.newBanner = newBanner
+const getOneBanner = async (req, res) => {
+  try {
+    const GoalBanners = await Banners.findById(req.params.id);
+    res.status(200).json(GoalBanners);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ msg: "error" });
+  }
+};
+
+module.exports = {
+  getAllBanner,
+  newBanner,
+  updateBanner,
+  deleteBanner,
+  getOneBanner,
+};
--- routes/bannerRoutes.js
+++ routes/bannerRoutes.js
@@ -1,9 +1,12 @@
-   const express = require("express");
-   const router = express();
+const express = require("express");
+const router = express();
 
-   const BannerCtrl = require("../controllers/bannerCtrl");
+const BannerCtrl = require("../controllers/bannerCtrl");
 
-   router.get("/banners",BannerCtrl.getAllBanner);
-   router.post("/newBanners",BannerCtrl.newBanner);
+router.get("/banners", BannerCtrl.getAllBanner); // تغییر در مسیر برای دریافت همه‌ی بنرها
+router.post("/newBanners", BannerCtrl.newBanner);
+router.post("/updateBanners", BannerCtrl.updateBanner);
+router.post("/deleteBanners", BannerCtrl.deleteBanner);
+router.get("/banners/:id", BannerCtrl.getOneBanner); // تغییر در مسیر برای دریافت بنر با آیدی مشخص
 
-   module.exports = router;
+module.exports = router;
